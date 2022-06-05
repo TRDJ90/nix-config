70af7164266bfd24027baaae072bdd918739696c
Thubie de Jong <thubie@Thubies-MacBook-Pro.local>
2022-06-05 22:18:23 +0200

    Added alacritty configuration

--- home.nix
+++ home.nix
@@ -9,7 +9,6 @@
     ripgrep
     
     # development tooling
-    # helix
     dotnet-sdk
     omnisharp-roslyn
     rustc
@@ -17,6 +16,7 @@
     rustfmt
     rust-analyzer
     clippy
+    rnix-lsp
   ];
 
   # TODO: Find out how to break this up in seperate files and import
@@ -60,6 +60,7 @@
     enable = true;
     settings = {
       theme = "everforest_dark";
+      
       editor = {
         line-number = "relative";
         mouse = true;
@@ -68,14 +69,17 @@
         auto-completion = true;
         rulers = [80];
       };
+      
       editor.cursor-shape = {
         insert = "bar";
         normal = "block";
         select = "block";
       };
+      
       editor.lsp = {
         display-messages = true;
       };
+      
       editor.whitespace.render = {
         space = "all";
         tab = "all";
@@ -83,7 +87,61 @@
       };
     };
   };
-  
+ 
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.padding.x = 5;
+      window.padding.y = 5;
+      window.opacity = 0.75;
+      window.decorations = "full";
+      window.dynamic_title = true;
+      
+      live_config_reload = true;
+      use_thin_strokes = true;
+      
+      cursor.style.shape = "Block";
+      cursor.style.blinking = "Always";
+      blink_interval = 500;
+      
+      font = {
+        size = 15;
+        normal.family = "Iosevka Nerd Font";
+        normal.style = "Light";
+        bold.family = "Iosevka Nerd Font";
+        bold.style = "Bold";
+        italic.family = "Iosevka Nerd Font";
+        italic.style = "Italic";
+      };
+      
+      colors = {
+        cursor.cursor = "#81a1c1";
+        primary.background = "#2b3339";
+        primary.foreground = "#d3c6aa";
+        normal = {
+          black =    "#4b565c";
+          red =      "#e67e80";
+          green =    "#a7c080";
+          yellow =   "#dbbc7f";
+          blue =     "#7fbbb3";
+          magenta =  "#d699b6";
+          cyan =     "#83c092";
+          white =    "#d3c6aa";
+        };
+        bright = {
+          black = "#4b565c";
+          red = "#e67e80";
+          green = "#a7c080";
+          yellow = "#dbbc7f";
+          blue = "#7fbbb3";
+          magenta = "#d699b6";
+          cyan = "#83c092";
+          white = "#d3c6aa";
+        };
+      };
+    };
+  };
+   
   programs.home-manager = {
     enable = true;
   };
